228a18562b34b53c6d548c98ae2fdafb05004789
rabiaaktas <aktasrabiaa@gmail.com>
2024-09-11 23:19:02 +0300

    fix action

--- playwright.config.ts
+++ playwright.config.ts
@@ -11,6 +11,9 @@ import { defineConfig, devices } from '@playwright/test';
  * See https://playwright.dev/docs/test-configuration.
  */
 export default defineConfig({
+  // Base URL to use in actions like `await page.goto('/')`.
+  //baseURL: 'http://127.0.0.1:3000',
+  colorScheme: 'dark',
   testDir: './tests',
   /* Run tests in files in parallel */
   fullyParallel: true,
